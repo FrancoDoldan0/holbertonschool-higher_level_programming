57b6b181c856831369fe5c1f4dd79c492191aa3d
FrancoDoldan0 <francodoldan23@gmail.com>
2024-01-26 17:02:34 -0800

    add task 2 fixed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-args = sys.argv[1:]
-arguments = str(args)
+    args = sys.argv[1:]
+    arguments = str(args)
 
-if len(args) == 0:
-    print(len(args), "arguments.", end="\n",)
-else:
-    print("{} {}{}".format(len(args), "arguments", ":"), end=("\n"))
-    for i, arg in enumerate(args, 1):
-        print("{}: {}".format(i, arg))
+    if len(args) == 0:
+        print(len(args), "arguments.", end="\n",)
+    else:
+        print("{} {}{}".format(len(args), "arguments", ":"), end=("\n"))
+        for i, arg in enumerate(args, 1):
+            print("{}: {}".format(i, arg))

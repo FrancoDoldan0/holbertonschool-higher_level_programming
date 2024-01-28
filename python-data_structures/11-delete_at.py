f369998fa3ae7e6fd8cb1b684f2b75ac79f64b84
FrancoDoldan0 <francodoldan23@gmail.com>
2024-01-28 13:31:19 -0800

    add task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0:
-        return (my_list)
 
     for i in my_list:
-        del my_list[3]
-        return (my_list)
+        if idx < 0:
+            return (my_list)
+        else:
+            del my_list[3]
+            return (my_list)

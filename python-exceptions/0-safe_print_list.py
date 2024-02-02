77d3537e97794f28a7b3b5e30d0bea8f98f844be
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-02 10:13:08 -0800

    add task 0 fixed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     a = 0
     for i in range(0, x):
         try:
-            print (my_list[i], end="")
+            print(my_list[i], end="")
             a += 1
         except Exception:
             break

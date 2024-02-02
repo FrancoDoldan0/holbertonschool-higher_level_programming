c51eb38bd64e3bb941f46ce1a00b56ca8cf62959
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-02 11:55:16 -0800

    add task 4 fixed

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,8 +2,8 @@
 def list_division(my_list_1, my_list_2, list_length):
 
     Newlist = []
-    div = 0
     for i in range(0, list_length):
+        div = 0
         try:
             div = my_list_1[i] / my_list_2[i]
         except TypeError:

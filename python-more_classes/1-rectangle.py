cea2e36224fba65ba2c138b61ff6e128eda47157
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-05 10:32:48 -0800

    add task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """void class"""
     def __init__(self, width=0, height=0):
-        self.width = height
-        self.height = width
+        self.height = height
+        self.width = width
 
     """width getter"""
     @property

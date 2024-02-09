cab9c81ec3cf9e01ff4954335aabaa8e8b5c86ac
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-09 07:25:44 -0800

    add task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class Rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """Rectangle function"""
+        self.__width = width
+        super().integer_validator("width", width)
+        self.__height = height
+        super().integer_validator("height", height)
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return (f"[rectangle] {self.__width}/{self.__height}")

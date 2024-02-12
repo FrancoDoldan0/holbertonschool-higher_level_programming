4bf4d6b5c123f12778ed9f9f688980978f5cc10b
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-12 10:19:52 -0800

    add task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,9 +6,9 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """init"""
+    """Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-
+        """init method"""
         super().__init__(id)
         self.width = width
         self.height = height

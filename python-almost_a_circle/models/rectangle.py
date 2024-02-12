8741fbecba11b961047f66644cde3ec11dddd116
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-12 10:43:13 -0800

    add task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,3 +14,37 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    """getters"""
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def y(self):
+        return self.__y
+
+    @property
+    def x(self):
+        return self.__x
+
+    """setters"""
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    @x.setter
+    def x(self, value):
+        self.__x = value

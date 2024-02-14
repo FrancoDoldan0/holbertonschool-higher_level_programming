d3e4d364b686c13c54149ea08a09f451f596740f
FrancoDoldan0 <francodoldan23@gmail.com>
2024-02-14 08:12:16 -0800

    add task 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,3 +91,17 @@ class Rectangle(Base):
         """Str Method"""
         str_a = f"({self.id}) {self.x}/{self.y}"
         return f"[Rectangle] {str_a} - {self.width}/{self.height}"
+
+    def update(self, *args):
+        """Update the attributtes using args"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
